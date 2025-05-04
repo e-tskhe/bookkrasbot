56dfa262ea353777faf2c20805093c9abf2b2b7f
e-tskhe <youngartist7009@gmail.com>
2025-05-04 14:51:33 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -20,3 +20,8 @@ def webhook():
 @app.route('/')
 def health_check():
     return "Bot is running!", 200
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
